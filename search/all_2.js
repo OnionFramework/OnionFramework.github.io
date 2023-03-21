4c9367844c56ede1eda082ae7fbed427f4b8800d
OnionFramework at Imac <onion.mhframework@gmail.com>
2023-03-21 08:46:01 -0300

    Added graphviz in macOS

--- search/all_2.js
+++ search/all_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['definitions_14',['Definitions',['../md__glossary.html',1,'']]],
-  ['description_15',['description',['../classonion_1_1_component_data.html#a4fd01fcf3664ecd76d8902eafefe13b8',1,'onion::ComponentData::description()'],['../classonion_1_1_i_d_builder.html#ac3db342592a4123c90ab18bb8b458324',1,'onion::IDBuilder::description()']]]
+  ['description_14',['description',['../classonion_1_1_component_data.html#a4fd01fcf3664ecd76d8902eafefe13b8',1,'onion::ComponentData::description()'],['../classonion_1_1_i_d_builder.html#ac3db342592a4123c90ab18bb8b458324',1,'onion::IDBuilder::description()']]],
+  ['definitions_15',['Definitions',['../md__glossary.html',1,'']]]
 ];
